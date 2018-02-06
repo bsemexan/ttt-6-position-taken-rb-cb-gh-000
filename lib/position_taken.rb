1eea7d2b774c4164b60cb9171e9fca57aa8bed3e
Berry  Semexan <bsemexan@gmail.com>
2018-02-06 21:38:48 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
+  return true if board[index] = "X" || board[index] = "O"
   return false if board[index] = ""
   return false if board[index] = " "
   return false if board[index] = nil
-  return true if board[index] = "X" || board[index] = "O"
 end
