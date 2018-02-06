0281d772d60541f2aaefe53d816915a9fd083d89
Berry  Semexan <bsemexan@gmail.com>
2018-02-06 21:54:27 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] = "X" || board[index] = "O"
     return true
-  elsif board[index] = "" || board[index] = " " || board[index] = nil
+  if board[index] = "" || board[index] = " " || board[index] = nil
     return false
   end
 end
